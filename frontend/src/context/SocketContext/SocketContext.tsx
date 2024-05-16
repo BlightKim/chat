b6fceaa58898a5e4802ddb9be34d4ce0fcefe7ef
sebin kim <ksebin96@gmail.com>
2024-05-16 09:39:53 +0900

    feat : 기존 프런트엔드 삭제

--- frontend/src/context/SocketContext/SocketContext.tsx
+++
@@ -1,6 +0,0 @@
-import {createContext} from "react";
-import {SocketContextProps} from "@/context/SocketContext/socketContext.types";
-
-const SocketContext = createContext<SocketContextProps>({});
-
-export {SocketContext}
